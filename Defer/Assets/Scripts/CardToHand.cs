62413a9ad06272c8b90139c1c035db746a667632
perskii <86572700+perskii@users.noreply.github.com>
2021-07-15 19:15:49 +0200

    All bugs are fixed, cards now have normal size
    desc
    image
    shff

--- Defer/Assets/Scripts/CardToHand.cs
+++ Defer/Assets/Scripts/CardToHand.cs
@@ -10,11 +10,15 @@ public class CardToHand : MonoBehaviour
     void Start()
     {
         Hand = GameObject.Find("Hand");
+        /*Vector3 vector3 ;*/
+/*         float pos1 = 2;
+         float pos2 = 2;
+         float pos3 = 1;*/
 
         //if (It.tag == "First")
         //{
         It.transform.SetParent(Hand.transform);
-        It.transform.localScale = Vector3.one;
+        It.transform.localScale = new Vector3(1.5f, 2.3f, 1.0f);
         It.transform.position = new Vector3(transform.position.x, transform.position.y, -48);
         It.transform.eulerAngles = new Vector3(25, 0, 0);
         It.tag = "Untagged";
