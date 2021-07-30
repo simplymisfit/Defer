ba0e8aea32eb93e30f5ff1269b821c43421a3517
msft <63207978+simplymisfit@users.noreply.github.com>
2021-07-30 11:02:47 +0200

    Added new scene with card collection
    x

--- Defer/Assets/Scripts/CardDatabase.cs
+++ Defer/Assets/Scripts/CardDatabase.cs
@@ -15,7 +15,9 @@ public class CardDatabase : MonoBehaviour
         cardList.Add(new Card(2, "Dwarf", 2, 300, 4, "Add 1 max mana", Resources.Load<Sprite>("1"), "Blue", 0, 1, 0, 0, false, 0));
         cardList.Add(new Card(3, "Human", 3, 400, 3, "Add 3 max mana", Resources.Load<Sprite>("1"), "Yellow", 0, 3, 0, 0, false, 0));
         cardList.Add(new Card(4, "Demon", 1, 500, 2, "Return 1 Card from graveyard", Resources.Load<Sprite>("1"), "Purple", 0, 0,1, 0, false, 0));
-        cardList.Add(new Card(5, "Lifestealer", 1, 500, 2, "Heal 1000", Resources.Load<Sprite>("1"), "Green", 0, 0, 1, 1000, false, 0));
+        cardList.Add(new Card(5, "Lifestealer", 1, 500, 2, "Heal 1000", Resources.Load<Sprite>("5"), "Green", 0, 0, 1, 1000, false, 0));
+
+        cardList.Add(new Card(6, "Lifeeater", 2, 500, 3, "Heal 3000", Resources.Load<Sprite>("6"), "Green", 0, 0, 1, 3000, false, 0));
 
         // spell cards
         cardList.Add(new Card(7, "FireBall", 4, 0,0, "Deal 3000 DMG to your opponent", Resources.Load<Sprite>("7"), "Red", 0, 0, 0, 0, true, 3000));
